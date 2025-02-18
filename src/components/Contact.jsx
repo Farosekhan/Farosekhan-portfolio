e08aef78d819c08787af0bc7cc6b7d5a83b6f3bc
Riyas <riyaskhan7897@gmail.com>
2025-02-18 21:05:33 +0530

    first commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
                   <path d="M32 0A24.032 24.032 0 0 0 8 24c0 17.23 22.36 38.81 23.31 39.72a.99.99 0 0 0 1.38 0C33.64 62.81 56 41.23 56 24A24.032 24.032 0 0 0 32 0zm0 35a11 11 0 1 1 11-11 11.007 11.007 0 0 1-11 11z" data-original="#000000" />
                 </svg>
                 <div>
-                  <h4 className="font-semibold text-gray-900 text-sm mb-1">My Location</h4>
+                  <h4 className="font-semibold text-gray-900 text-sm mb-1">My Location:</h4>
                   <p className="text-gray-600 text-sm">65 D/2 Poosari Street Chinthamani Trichy-2</p>
                   <p className="text-gray-600 text-sm"> TamilNadu India .</p>
                 </div>
